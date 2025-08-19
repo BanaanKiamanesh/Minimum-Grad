1ee35992a1125a0b03bb0c05dd2b461666d93697
Banaan Kiamanesh <kiamaneshbanaan@gmail.com>
2025-08-19 15:37:38 +0200

    BackProp Tensor Implementation + a Simple Test Script

---
+++ src/Tensor.m
@@ -0,0 +1,319 @@
+classdef Tensor < handle
+    % Simple AutoGrad Tensor with Broadcasting-Aware Back propagation.
+    % Supports: sum, log, exp, unary -, +, -, .*, *, .^, slicing, backward.
+
+    properties
+        Data
+        RequireGrad (1, 1) logical = false
+        DependsOn                            % Cell Array of Structs('Tensor', Tensor, 'GradFun', @f)
+        Grad                                 % Tensor (Same Shape as Data)
+        Shape                                % Size Vector
+    end
+
+    methods
+        function obj = Tensor(Data, RequireGrad, DependsOn)
+            if nargin < 1
+                Data = [];
+            end
+
+            obj.Data = Tensor.EnsureArray(Data);
+            if nargin >= 2 && ~isempty(RequireGrad)
+                obj.RequireGrad = logical(RequireGrad);
+            end
+
+            if nargin >= 3 && ~isempty(DependsOn)
+                obj.DependsOn = DependsOn;
+            else
+                obj.DependsOn = {};
+            end
+
+            obj.Shape = size(obj.Data);
+            obj.Grad  = [];
+            if obj.RequireGrad
+                obj.ZeroGrad();
+            end
+        end
+
+        function ZeroGrad(obj)
+            obj.Grad = Tensor(zeros(size(obj.Data)));
+        end
+
+        function Out = sum(T, varargin)
+            Data = sum(T.Data, 'all');
+            Req  = T.RequireGrad;
+
+            if Req
+                Dep = {struct('Tensor', T, 'GradFun', @(g) g .* ones(size(T.Data)))};
+            else
+                Dep = {};
+            end
+
+            Out = Tensor(Data, Req, Dep);
+        end
+
+        function Out = log(T)
+            Data = log(T.Data);
+            Req  = T.RequireGrad;
+
+            if Req
+                dep = {struct('Tensor', T, 'GradFun', @(g) g ./ T.Data)};
+            else
+                dep = {};
+            end
+
+            Out = Tensor(Data, Req, dep);
+        end
+
+        function Out = exp(T)
+            Data = exp(T.Data);
+            Req  = T.RequireGrad;
+
+            if Req
+                Dep = {struct('Tensor', T, 'GradFun', @(g) g .* Data)};
+            else
+                Dep = {};
+            end
+
+            Out = Tensor(Data, Req, Dep);
+        end
+
+        function Out = uminus(T)
+            T = Tensor.EnsureTensor(T);
+            Data = -T.Data;
+            Req  =  T.RequireGrad;
+
+            if Req
+                Dep = {struct('Tensor', T, 'GradFun', @(g) -g)};
+            else
+                Dep = {};
+            end
+
+            Out = Tensor(Data, Req, Dep);
+        end
+
+        function Out = plus(T1, T2)
+            T1 = Tensor.EnsureTensor(T1);
+            T2 = Tensor.EnsureTensor(T2);
+
+            Data = T1.Data + T2.Data;
+            Req  = T1.RequireGrad || T2.RequireGrad;
+            Dep  = {};
+
+            if T1.RequireGrad
+                Dep{end + 1} = struct('Tensor', T1, 'GradFun', @(g) Tensor.ReduceGrad(g, size(T1.Data)));
+            end
+            if T2.RequireGrad
+                Dep{end + 1} = struct('Tensor', T2, 'GradFun', @(g) Tensor.ReduceGrad(g, size(T2.Data)));
+            end
+
+            Out = Tensor(Data, Req, Dep);
+        end
+
+        function Out = minus(T1, T2)
+            T1 = Tensor.EnsureTensor(T1);
+            T2 = Tensor.EnsureTensor(T2);
+
+            Out = plus(T1, uminus(T2));
+        end
+
+        function Out = times(T1, T2)
+            T1 = Tensor.EnsureTensor(T1);
+            T2 = Tensor.EnsureTensor(T2);
+
+            Data = T1.Data .* T2.Data;
+            Req  = T1.RequireGrad || T2.RequireGrad;
+            Dep  = {};
+
+            if T1.RequireGrad
+                Dep{end + 1} = struct('Tensor', T1, 'GradFun', @(g) Tensor.ReduceGrad(g .* T2.Data, size(T1.Data)));
+            end
+
+            if T2.RequireGrad
+                Dep{end + 1} = struct('Tensor', T2, 'GradFun', @(g) Tensor.ReduceGrad(g .* T1.Data, size(T2.Data)));
+            end
+
+            Out = Tensor(Data, Req, Dep);
+        end
+
+        function Out = mtimes(T1, T2)
+            T1 = Tensor.EnsureTensor(T1);
+            T2 = Tensor.EnsureTensor(T2);
+
+            Data = T1.Data * T2.Data;
+            Req = T1.RequireGrad || T2.RequireGrad;
+            Dep = {};
+
+            if T1.RequireGrad
+                Dep{end + 1} = struct('Tensor', T1, 'GradFun', @(g) g * T2.Data.');
+            end
+
+            if T2.RequireGrad
+                Dep{end + 1} = struct('Tensor', T2, 'GradFun', @(g) T1.Data.' * g);
+            end
+
+            Out = Tensor(Data, Req, Dep);
+        end
+
+        function Out = power(T, p)
+            T = Tensor.EnsureTensor(T);
+
+            if isa(p, 'Tensor')
+                error('Exponent must be numeric for this simple autograd.');
+            end
+
+            if isscalar(p) && p == -1
+                Data = 1 ./ T.Data;
+            else
+                Data = T.Data .^ p;
+            end
+
+            Req = T.RequireGrad;
+            if Req
+                if isscalar(p) && p == -1
+                    GradF = @(g) g .* (-1) .* (T.Data .^ -2);
+                else
+                    GradF = @(g) g .* p .* (T.Data .^ (p - 1));
+                end
+                Dep = {struct('Tensor', T, 'GradFun', GradF)};
+            else
+                Dep = {};
+            end
+
+            Out = Tensor(Data, Req, Dep);
+        end
+
+        function varargout = subsref(T, S)
+            if strcmp(S(1).type, '()')
+                idx  = S(1).subs;
+                Data = T.Data(idx{:});
+                Req  = T.RequireGrad;
+
+                if Req
+                    GradFun = @(g) Tensor.SliceBackProp(g, size(T.Data), idx);
+                    Dep = {struct('Tensor', T, 'GradFun', GradFun)};
+                else
+                    Dep = {};
+                end
+                Out = Tensor(Data, Req, Dep);
+
+                if numel(S) > 1
+                    [varargout{1:nargout}] = builtin('subsref', Out, S(2:end));
+                else
+                    if nargout > 0
+                        varargout{1} = Out;
+                    else
+                        assignin('caller', 'ans', Out);
+                    end
+                end
+            else
+                [varargout{1:nargout}] = builtin('subsref', T, S);
+            end
+        end
+
+        function Backward(T, Grad)
+            if nargin < 2 || isempty(Grad)
+                if isscalar(T.Data)
+                    Grad = Tensor(1.0);
+                else
+                    error('grad must be specified for non-scalar tensor');
+                end
+            else
+                Grad = Tensor.EnsureTensor(Grad);
+            end
+
+            if isempty(T.Grad)
+                T.Grad = Tensor(zeros(size(T.Data)));
+            end
+
+            T.Grad.Data = T.Grad.Data + Grad.Data;
+
+            % Recursively Take the Grad Down to the Last Leaves
+            for k = 1:numel(T.DependsOn)
+                Dep = T.DependsOn{k};
+                BackGrad = Dep.GradFun(Grad.Data);
+                Dep.Tensor.Backward(Tensor(BackGrad));
+            end
+        end
+    end
+
+    methods (Static, Access = private)
+        function a = EnsureArray(A)
+            if isa(A, 'Tensor')
+                a = A.Data;
+            else
+                a = double(A);
+            end
+        end
+
+        function t = EnsureTensor(T)
+            if isa(T, 'Tensor')
+                t = T;
+            else
+                t = Tensor(T);
+            end
+        end
+
+        function g = ReduceGrad(Grad, ShapeOriginal)
+            g  = Grad;
+
+            go = size(g);
+            so = ShapeOriginal;
+
+            Lg = numel(go);
+            La = numel(so);
+            
+            DimAdded = max(Lg - La, 0);
+
+            if DimAdded > 0
+                go = size(g);
+                if numel(go) < DimAdded
+                    go(end + 1:DimAdded) = 1; 
+                end
+                
+                LeadProd = prod(go(1:DimAdded));
+                Rest     = go(DimAdded+1:end);
+                
+                if isempty(Rest)
+                    Rest = 1; 
+                end
+                
+                g = reshape(g, [LeadProd, Rest]);
+                g = sum(g, 1);
+                g = reshape(g, [ones(1, DimAdded), Rest]);
+            end
+
+            go = size(g);
+            La = numel(ShapeOriginal);
+            if numel(go) < La
+                go(end + 1:La) = 1; 
+            end
+
+            for i = 1:La
+                DimA = ShapeOriginal(i);
+                DimG = go(i);
+                if DimA == 1 && DimG ~= 1
+                    g  = sum(g, i);
+                    sz = size(g);
+                    if numel(sz) < i
+                        sz(end + 1:i) = 1; 
+                    end
+                    
+                    if sz(i) ~= 1
+                        sz(i) = 1;
+                        g = reshape(g, sz);
+                    end
+
+                    go = size(g);
+                    if numel(go) < La
+                        go(end + 1:La) = 1;
+                    end
+                end
+            end
+        end
+
+        function Bigger = SliceBackProp(Grad, FullSize, idx)
+            Bigger = zeros(FullSize);
+            Bigger(idx{:}) = Grad;
+        end
+    end
+end
---
+++ test/TestTensor.m
@@ -0,0 +1,174 @@
+clear
+close all
+clc
+rng(0)
+
+addpath("../src/")
+%% Init Test
+
+tol = 1e-10;
+AssertClose = @(A, B, msg) assert(max(abs(A(:) - B(:))) < tol, msg);
+
+%% Basic Ops
+% sum
+x = Tensor(rand(2, 3), true);
+s = x.sum();
+s.Backward();
+
+AssertClose(x.Grad.Data, ones(2, 3), 'sum/backprop failed');
+
+% log
+x = Tensor(rand(2, 3) + 1.0, true);
+y = x.log();
+y = y.sum();
+y.Backward();
+
+AssertClose(x.Grad.Data, 1 ./ x.Data, 'log grad wrong');
+
+% exp
+x = Tensor(rand(2, 3), true);
+y = x.exp();
+y = y.sum();
+y.Backward();
+
+AssertClose(x.Grad.Data, exp(x.Data), 'exp grad wrong');
+
+% uminus
+x = Tensor(rand(2, 2), true);
+y = -x;
+y = y.sum();
+y.Backward();
+
+AssertClose(x.Grad.Data, -ones(2, 2), 'uminus grad wrong');
+
+%%  Addition & Subtraction with Broadcasting
+a = Tensor(rand(2, 3), true);
+b = Tensor(rand(1, 3), true);
+z = a + b;
+z = z.sum();
+z.Backward();
+
+AssertClose(a.Grad.Data,   ones(2, 3), '+ grad (a) wrong');
+AssertClose(b.Grad.Data, 2*ones(1, 3), '+ grad (b) wrong');
+
+a = Tensor(rand(2, 3), true);
+b = Tensor(rand(1, 3), true);
+z = a - b;
+z = z.sum();
+z.Backward();
+
+AssertClose(a.Grad.Data,    ones(2, 3), '- grad (a) wrong');
+AssertClose(b.Grad.Data, -2*ones(1, 3), '- grad (b) wrong');
+
+%% Edge Case: Column Vector Broadcast
+a = Tensor(rand(4, 5), true);
+b = Tensor(rand(4, 1), true);
+z = a + b;
+z = z.sum();
+z.Backward();
+
+AssertClose(b.Grad.Data, sum(ones(4, 5), 2), 'broadcast col grad wrong');
+
+%%  Multiplication (Elementwise)
+a = Tensor(rand(2, 3), true);
+b = Tensor(rand(1, 3), true);
+z = a .* b;
+z = z.sum();
+z.Backward();
+
+AssertClose(a.Grad.Data, repmat(b.Data, 2, 1), '.* grad (a) wrong');
+AssertClose(b.Grad.Data,       sum(a.Data, 1), '.* grad (b) wrong');
+
+%%  Matrix Multiply
+A = Tensor(rand(2, 3), true);
+B = Tensor(rand(3, 4), true);
+S = A * B;
+S = S.sum();
+S.Backward();
+
+expectA = ones(2, 4) * B.Data.';
+expectB = A.Data.' * ones(2, 4);
+
+AssertClose(A.Grad.Data, expectA, '* grad (A) wrong');
+AssertClose(B.Grad.Data, expectB, '* grad (B) wrong');
+
+%% Rectangular MatMul
+A = Tensor(rand(5, 2), true);
+B = Tensor(rand(2, 1), true);
+S = A * B;
+S = S.sum();
+S.Backward();
+
+AssertClose(A.Grad.Data, repmat(B.Data.', 5, 1), 'rect mmul grad A wrong');
+AssertClose(B.Grad.Data,       sum(A.Data, 1).', 'rect mmul grad B wrong');
+
+%%  Power
+x = Tensor(rand(2, 3), true);
+p = 3;
+z = x.^p;
+z = z.sum();
+z.Backward();
+
+AssertClose(x.Grad.Data, p * (x.Data .^ (p-1)), '.^ grad wrong');
+
+%% Reciprocal
+x = Tensor(rand(2, 3) + 1, true);
+z = x.^-1;
+z = z.sum();
+z.Backward();
+
+AssertClose(x.Grad.Data, -1 .* (x.Data .^ -2), 'reciprocal grad wrong');
+
+%%  Slicing
+x = Tensor(reshape(1:12, 3, 4), true);
+y = x(2:3, 2:4);
+y = y.sum();
+y.Backward();
+
+expect = zeros(3, 4);
+expect(2:3, 2:4) = 1;
+
+AssertClose(x.Grad.Data, expect, 'slice grad wrong');
+
+%% Slice Single Element
+x = Tensor(magic(3), true);
+y = x(2, 2);
+y.Backward();
+
+expect = zeros(3, 3);
+expect(2, 2) = 1;
+
+AssertClose(x.Grad.Data, expect, 'single slice grad wrong');
+
+%%  Explicit Grads
+x = Tensor(rand(2, 2), true);
+y = x + 1;
+g = Tensor([1 2; 3 4]);
+y.Backward(g);
+
+AssertClose(x.Grad.Data, g.Data, 'explicit grad wrong');
+
+%%  Numeric + Tensor Mixing
+x = Tensor(rand(2, 3), true);
+y = 3 + x;
+y = y.sum();
+y.Backward();
+
+AssertClose(x.Grad.Data, ones(2, 3), 'numeric + Tensor grad wrong');
+
+x = Tensor(rand(2, 3), true);
+y = x + 3;
+y = y.sum();
+y.Backward();
+
+AssertClose(x.Grad.Data, ones(2, 3), 'Tensor + numeric grad wrong');
+
+%%  Chain Rule Sanity
+x = Tensor(rand(2, 2), true);
+y = x .* x;
+y = y.sum();
+y.Backward();
+
+AssertClose(x.Grad.Data, 2 .* x.Data, 'chain rule 2x failed');
+
+disp('All Tests Passed!');
